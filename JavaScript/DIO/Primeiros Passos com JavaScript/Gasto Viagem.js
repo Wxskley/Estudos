f0955b577396fd1419fbe9c5c6c8e92328a16b9f
LeanWeskley <weskleydsilva@yahoo.com.br>
2022-11-10 15:45:34 -0300

    Correção

--- JavaScript/DIO/Primeiros Passos com JavaScript/Gasto Viagem.js
+++
@@ -1,4 +0,0 @@
-let precoCombustivel = 5.37;
-let consumoMedioCarro = 14;
-let distanciaViagem = 600;
-console.log(distanciaViagem / consumoMedioCarro * precoCombustivel)
---
+++ JavaScript/DIO/Primeiros Passos com JavaScript/GastoViagem.js
@@ -0,0 +1,4 @@
+let precoCombustivel = 5.79;
+let consumoMedioCarro = 10;
+let distanciaViagem = 100;
+console.log(distanciaViagem / consumoMedioCarro * precoCombustivel);
