c5d5627e74281f6dfb008752fb2ed5a4b9050aec
LeanWeskley <weskleydsilva@yahoo.com.br>
2022-11-15 20:22:00 -0300

    aula 5 mod 2

--- JavaScript/Pokedex/assets/js/main.js
+++ JavaScript/Pokedex/assets/js/main.js
@@ -1,6 +1,4 @@
-const offset = 0;
-const limit = 10;
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+
 function convertPokemonToLi(pokemon) {
     return `
         <li class="pokemon">
@@ -18,14 +16,11 @@ function convertPokemonToLi(pokemon) {
     `
 }
 const pokemonList = document.getElementById('pokemonList')
-
-fetch(url)
-    .then((response) => response.json())
-    .then((jsonBody) => jsonBody.results)
-    .then((pokemons) => {
-        for (let i = 0; i < pokemons.length; i++) {
-            const pokemon = pokemons[i];
-            pokemonList.innerHTML += convertPokemonToLi(pokemon)
-        }
-    })
-    .catch((error) => console.log(error))
+pokeApi.getPokemons().then((pokemons) => {
+    const listItens = []
+    for (let i = 0; i < pokemons.length; i++) {
+        const pokemon = pokemons[i];
+        listItens.push(convertPokemonToLi(pokemon))
+    }
+    console.log(listItens)
+})
