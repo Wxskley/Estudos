8135e57e1a6c9cfa59be27e4a565ee7932f5d947
LeanWeskley <weskleydsilva@yahoo.com.br>
2022-11-16 08:37:25 -0300

    aula 8 mod 2

--- JavaScript/Pokedex/assets/js/poke-api.js
+++ JavaScript/Pokedex/assets/js/poke-api.js
@@ -1,16 +1,21 @@
 const pokeApi = {}
-pokeApi.getPokemons = (offset = 0, limit = 10) => {
+pokeApi.getPodemonDetail = (pokemon) => {
+    return fetch(pokemon.url).then((response) => response.json())
+}
+pokeApi.getPokemons = (offset = 1, limit = 5) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
     return fetch(url)
         .then((response) => response.json())
         .then((jsonBody) => jsonBody.results)
-        .catch((error) => console.log(error))
+        .then((pokemons) => pokemons.map(pokeApi.getPodemonDetail))
+        .then((detailRequest) => Promise.all(detailRequest))
+        .then((pokemonsDetails) => pokemonsDetails)
 }
 Promise.all([
     fetch('https://pokeapi.co/api/v2/pokemon/1'),
     fetch('https://pokeapi.co/api/v2/pokemon/2'),
     fetch('https://pokeapi.co/api/v2/pokemon/3'),
     fetch('https://pokeapi.co/api/v2/pokemon/4'),
-]).then((results) =>{
+]).then((results) => {
     console.log(results)
 })
